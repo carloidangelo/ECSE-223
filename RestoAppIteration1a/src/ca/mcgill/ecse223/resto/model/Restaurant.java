5c731a7cbfb512f408009bd4fe28c1daeb85f376
Yiming Yao <yiming.yao@mail.mcgill.ca>
2018-01-28 15:25:22 -0500

    Initial commit

---
+++ RestoAppIteration1a/src/ca/mcgill/ecse223/resto/model/Restaurant.java
@@ -0,0 +1,146 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.27.0.3728.d139ed893 modeling language!*/
+
+package ca.mcgill.ecse223.resto.model;
+import java.util.*;
+
+// line 88 "../../../../../RestoApp1a.ump"
+public class Restaurant
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Restaurant Attributes
+  private String restaurantName;
+
+  //Restaurant Associations
+  private List<Table> tabling;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Restaurant(String aRestaurantName)
+  {
+    restaurantName = aRestaurantName;
+    tabling = new ArrayList<Table>();
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setRestaurantName(String aRestaurantName)
+  {
+    boolean wasSet = false;
+    restaurantName = aRestaurantName;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public String getRestaurantName()
+  {
+    return restaurantName;
+  }
+
+  public Table getTabling(int index)
+  {
+    Table aTabling = tabling.get(index);
+    return aTabling;
+  }
+
+  public List<Table> getTabling()
+  {
+    List<Table> newTabling = Collections.unmodifiableList(tabling);
+    return newTabling;
+  }
+
+  public int numberOfTabling()
+  {
+    int number = tabling.size();
+    return number;
+  }
+
+  public boolean hasTabling()
+  {
+    boolean has = tabling.size() > 0;
+    return has;
+  }
+
+  public int indexOfTabling(Table aTabling)
+  {
+    int index = tabling.indexOf(aTabling);
+    return index;
+  }
+
+  public static int minimumNumberOfTabling()
+  {
+    return 0;
+  }
+
+  public boolean addTabling(Table aTabling)
+  {
+    boolean wasAdded = false;
+    if (tabling.contains(aTabling)) { return false; }
+    tabling.add(aTabling);
+    wasAdded = true;
+    return wasAdded;
+  }
+
+  public boolean removeTabling(Table aTabling)
+  {
+    boolean wasRemoved = false;
+    if (tabling.contains(aTabling))
+    {
+      tabling.remove(aTabling);
+      wasRemoved = true;
+    }
+    return wasRemoved;
+  }
+
+  public boolean addTablingAt(Table aTabling, int index)
+  {  
+    boolean wasAdded = false;
+    if(addTabling(aTabling))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTabling()) { index = numberOfTabling() - 1; }
+      tabling.remove(aTabling);
+      tabling.add(index, aTabling);
+      wasAdded = true;
+    }
+    return wasAdded;
+  }
+
+  public boolean addOrMoveTablingAt(Table aTabling, int index)
+  {
+    boolean wasAdded = false;
+    if(tabling.contains(aTabling))
+    {
+      if(index < 0 ) { index = 0; }
+      if(index > numberOfTabling()) { index = numberOfTabling() - 1; }
+      tabling.remove(aTabling);
+      tabling.add(index, aTabling);
+      wasAdded = true;
+    } 
+    else 
+    {
+      wasAdded = addTablingAt(aTabling, index);
+    }
+    return wasAdded;
+  }
+
+  public void delete()
+  {
+    tabling.clear();
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "restaurantName" + ":" + getRestaurantName()+ "]";
+  }
+}
---
+++ RestoAppIteration1a/src/ca/mcgill/ecse223/resto/model/Seat.java
@@ -0,0 +1,121 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.27.0.3728.d139ed893 modeling language!*/
+
+package ca.mcgill.ecse223.resto.model;
+
+// line 14 "../../../../../RestoApp1a.ump"
+public class Seat
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //Seat Attributes
+  private boolean isOccupied;
+
+  //Seat Associations
+  private Customer seating;
+  private Table assignedTo;
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public Seat(boolean aIsOccupied, Customer aSeating, Table aAssignedTo)
+  {
+    isOccupied = aIsOccupied;
+    if (aSeating == null || aSeating.getSittingIn() != null)
+    {
+      throw new RuntimeException("Unable to create Seat due to aSeating");
+    }
+    seating = aSeating;
+    boolean didAddAssignedTo = setAssignedTo(aAssignedTo);
+    if (!didAddAssignedTo)
+    {
+      throw new RuntimeException("Unable to create seating due to assignedTo");
+    }
+  }
+
+  public Seat(boolean aIsOccupied, String aNameForSeating, String aPhoneNumberForSeating, String aEmailAddressForSeating, boolean aHasPaidForSeating, boolean aHasReservationForSeating, Reservation aBookedForSeating, Table aAssignedTo)
+  {
+    isOccupied = aIsOccupied;
+    seating = new Customer(aNameForSeating, aPhoneNumberForSeating, aEmailAddressForSeating, aHasPaidForSeating, aHasReservationForSeating, this, aBookedForSeating);
+    boolean didAddAssignedTo = setAssignedTo(aAssignedTo);
+    if (!didAddAssignedTo)
+    {
+      throw new RuntimeException("Unable to create seating due to assignedTo");
+    }
+  }
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public boolean setIsOccupied(boolean aIsOccupied)
+  {
+    boolean wasSet = false;
+    isOccupied = aIsOccupied;
+    wasSet = true;
+    return wasSet;
+  }
+
+  public boolean getIsOccupied()
+  {
+    return isOccupied;
+  }
+
+  public Customer getSeating()
+  {
+    return seating;
+  }
+
+  public Table getAssignedTo()
+  {
+    return assignedTo;
+  }
+
+  public boolean setAssignedTo(Table aAssignedTo)
+  {
+    boolean wasSet = false;
+    if (aAssignedTo == null)
+    {
+      return wasSet;
+    }
+
+    Table existingAssignedTo = assignedTo;
+    assignedTo = aAssignedTo;
+    if (existingAssignedTo != null && !existingAssignedTo.equals(aAssignedTo))
+    {
+      existingAssignedTo.removeSeating(this);
+    }
+    assignedTo.addSeating(this);
+    wasSet = true;
+    return wasSet;
+  }
+
+  public void delete()
+  {
+    Customer existingSeating = seating;
+    seating = null;
+    if (existingSeating != null)
+    {
+      existingSeating.delete();
+    }
+    Table placeholderAssignedTo = assignedTo;
+    this.assignedTo = null;
+    if(placeholderAssignedTo != null)
+    {
+      placeholderAssignedTo.removeSeating(this);
+    }
+  }
+
+
+  public String toString()
+  {
+    return super.toString() + "["+
+            "isOccupied" + ":" + getIsOccupied()+ "]" + System.getProperties().getProperty("line.separator") +
+            "  " + "seating = "+(getSeating()!=null?Integer.toHexString(System.identityHashCode(getSeating())):"null") + System.getProperties().getProperty("line.separator") +
+            "  " + "assignedTo = "+(getAssignedTo()!=null?Integer.toHexString(System.identityHashCode(getAssignedTo())):"null");
+  }
+}
